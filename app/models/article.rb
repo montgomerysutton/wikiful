22225b827cbd339b9bb7174ee85c5ca2ce3fbf11
Montgomery Sutton <montgomery@lupoblu.com>
2015-01-09 17:38:41 -0500

    Add Category Model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ActiveRecord::Base
     has_many :article_categories
     has_many :categories, through: :article_categories
     
-    validates_presence_of :title,
-    validates_presence_of :content,
+    validates_presence_of :title
+    validates_presence_of :content
     validates_presence_of :categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109222812) do
+ActiveRecord::Schema.define(version: 20150109223218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20150109222812) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
