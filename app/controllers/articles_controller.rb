a49c682eca6f89d0cda5d6dfdafd90a3b8d90992
Montgomery Sutton <montgomery@lupoblu.com>
2015-01-13 09:33:03 -0500

    Add User Stories file; Add ArticlesController and Index, New, and Show views for Articles; Update routes.rb to includes resources for :articles.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+	def index
+
+	end
+
+	def show
+
+	end
+
+	def new
+
+	end
+
+	def create
+
+	end
+
+	def update
+
+	end
+
+	def destory
+
+	end
+
+end
